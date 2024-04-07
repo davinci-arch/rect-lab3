69117b70d5dd8a5f7a4762f1757c85c1db3cf87b
parfesa.a15@gmail.com <parfesa.a15@gmail.com>
2024-04-07 11:29:38 +0300

    [fetch]: header developed

---
+++ src/components/Container.jsx
@@ -0,0 +1,27 @@
+
+export default function Content() {
+
+    return (
+        <div className="w-full float-left bg-[#3c40c6] block bg-[url('https://flatuicolors.com/static/img/stars-opacity.0979c1.svg')]">
+            <div className="w-[1024px] my-0 mx-auto">
+                <div className="block w-[1024px] text-left relative h-[65px] box-border mb-[15px]">
+                    <div className="flex justify-center float-left h-[105px] w-full">
+                        <a href="#" className="text-[24px] text-white absolute left-[2px] bottom-[5px] tracking-[2.5px] font-semibold select-none origin-[0%_50%]">FLAT UI COLORS 2</a>
+                        <div className="absolute right-0 bottom-[5px] mr-[2px] font-bold tracking-[1px] ">
+                            <a href="#" className="ml-[40px] font-semibold text-[17px] text-white inline-block relative">
+                                Designer Inspiration
+                                <span className="bg-[#FF0000] text-[14px] font-bold py-[2px] px-[4px] pl-[6px] rounded-[4px] ml-[8px]">NEW</span>
+                            </a>
+                            <a href="#" className="ml-[40px] font-semibold text-[17px] text-white inline-block relative">
+                                Subscribe
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    )
+
+}
